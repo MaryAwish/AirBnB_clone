36a4be367f07043d00d3bf3867d192eef60b3852
Mary Ouma <maryawish54@gmail.com>
2023-02-12 12:13:51 -0800

    exception

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,5 +49,5 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception as error:
             pass
